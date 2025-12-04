7330fcf3ba90cf5e373079fb6e07c25f4b378ef2
Khairul Makirin <khairulmmakirin@gmail.com>
2025-12-04 15:42:04 +0900

    added min radius and occupied neighbors cell dist parameters to the frontiers request service

--- srvs/get_frontiers.hpp
+++ srvs/get_frontiers.hpp
@@ -3,11 +3,13 @@
 namespace GetFrontiers {
     class Request {
         public:
-            float x_min, x_max;
+            float x_min, x_max; // x, y, z bounds for searching
             float y_min, y_max;
             float z_min, z_max;
+            float min_radius_from_robot; // frontier points must be a radius away from the robots
+            int occupied_neighbors_cell_dist; // there should be no occupied cells within cell_dist of the frontier points
 
-        MSGPACK_DEFINE(x_min, x_max, y_min, y_max, z_min, z_max);
+        MSGPACK_DEFINE(x_min, x_max, y_min, y_max, z_min, z_max, min_radius_from_robot, occupied_neighbors_cell_dist);
     };
 
     class Response {
